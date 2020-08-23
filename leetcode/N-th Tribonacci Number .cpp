ea5c07868109c5dba4fc4b46e17b987719a9626b
Sachin Soni <42809714+soni-sachin@users.noreply.github.com>
2020-08-23 18:28:40 +0530

    Update N-th Tribonacci Number .cpp

--- leetcode/N-th Tribonacci Number .cpp
+++ leetcode/N-th Tribonacci Number .cpp
@@ -1,12 +1,15 @@
-int tribonacci(int n) {
+class Solution {
+public:
+    int tribonacci(int n) {
         int first=0,second=1,third=1;
         if(n<2)
             return n;
         for(int i=3;i<=n;i++){
-            int newElement=first+second+third;
+            int nn=first+second+third;
             first=second;
             second=third;
-            third=newElement;
+            third=nn;
         }
         return third;
     }
+};
