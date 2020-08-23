86628de2245e87415c40fb723ede87327b77e0a7
Sachin Soni <42809714+soni-sachin@users.noreply.github.com>
2020-08-23 17:21:44 +0530

    Create square of sorted array .cpp

---
+++ leetcode/square of sorted array .cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int > result;
+        for(int i=0;i<A.size();i++){
+            result.push_back(A[i]*A[i]);
+        }
+        sort(result.begin(),result.end());
+        return result;
+    }
+};
